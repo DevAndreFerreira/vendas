dc916d5c920d49cb14b5a668ab0c2a8f6c6992c5
André Ferreira <andredepaulacontato@gmail.com>
2022-06-17 17:25:34 -0300

    Primeiro commit

---
+++ src/main/java/io/github/devandreferreira/MinhaConfiguration.java
@@ -0,0 +1,16 @@
+package io.github.devandreferreira;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+	
+//	@Bean(name = "applicationName")
+//	public String applicationName() {
+//		return "Sistema de Vendas";
+//	}
+	
+	
+	
+	
+}

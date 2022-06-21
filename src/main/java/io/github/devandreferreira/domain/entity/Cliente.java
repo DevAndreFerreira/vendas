5fd693c505fe4eb559898f459f73ee981a2d7e5e
André Ferreira <andredepaulacontato@gmail.com>
2022-06-20 22:05:35 -0300

    Criação das classes de domínio

---
+++ src/main/java/io/github/devandreferreira/domain/entity/Cliente.java
@@ -0,0 +1,15 @@
+package io.github.devandreferreira.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+
+
+}
---
+++ src/main/java/io/github/devandreferreira/domain/entity/Pedido.java
@@ -0,0 +1,18 @@
+package io.github.devandreferreira.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Pedido {
+
+    private Integer id;
+    private Cliente cliente;
+    private LocalDate dataPedido;
+    private BigDecimal total;
+
+}
